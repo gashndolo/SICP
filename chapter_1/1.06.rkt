6fb4e09a854bd370fe49664fd693b7e83be6bcef
Gershom Ndolo <gashadogo@gmail.com>
2020-11-20 13:18:26 +0300

    Chapter 1 > The elements of programming

--- chapter_1/1.06.rkt
+++ chapter_1/1.06.rkt
@@ -3,7 +3,7 @@
 ; Remember that the if special form will evaluate the predicate then only evaluate either the then-clause or the else-clause (but not both) 
 ; The new-if procedure is not a special form.
 ; Therefore all parts of the procedure will be evaluated,
-; This means that the clauses will all be evaluated and new-if will constantly call itself
+; This means that the clauses will all be evaluated and new-if will constantly call itself from the recursive call to sqrt-iter
 ; consider (sqrt 9)
 ; (new-if (good-enough? 1.0 9)
 ;         1.0
