cb5010f432413373e9c41321f5e8c9714f237fb5
Gershom Ndolo <gashadogo@gmail.com>
2020-11-20 13:16:48 +0300

    Chapter 1 > The elements of programming

---
+++ chapter_1/1.06.rkt
@@ -0,0 +1,10 @@
+#lang racket
+; Using the new-if, the program runs out of memory and is unable to execute
+; Remember that the if special form will evaluate the predicate then only evaluate either the then-clause or the else-clause (but not both) 
+; The new-if procedure is not a special form.
+; Therefore all parts of the procedure will be evaluated,
+; This means that the clauses will all be evaluated and new-if will constantly call itself
+; consider (sqrt 9)
+; (new-if (good-enough? 1.0 9)
+;         1.0
+;         (sqrt-iter (improve 1.0 9) 9)) ;The predicate (good-enough? 1.0 9) 
